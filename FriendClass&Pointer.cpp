ec24a53c927fc1da80e12351d99eed206b87d04b
variannn340 <varianvarian0301@gmail.com>
2026-05-05 11:29:43 +0700

    add method input in class LayangLayang

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -4,8 +4,14 @@ using namespace std;
 class BelahKetupat; //first declaration
 
 class LayangLayang {
-privae:
+private:
     float d1, d2, s1, s2;
 
+public:
+    void input() {
+        cin >> d1 >> d2 >> s1 >> s2;
+    }
+
     
-}
+
+};

f365f574e2ade18f8d52275610b829aae6910bc4
variannn340 <varianvarian0301@gmail.com>
2026-05-05 10:42:14 +0700

    full class pelajar

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -8,3 +8,17 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}

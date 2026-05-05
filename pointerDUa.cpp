43465b493874c3348805280618e49e18f47f09c5
variannn340 <varianvarian0301@gmail.com>
2026-05-05 10:56:51 +0700

    full int main()

--- pointerDUa.cpp
+++ pointerDUa.cpp
@@ -10,3 +10,12 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main ()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
